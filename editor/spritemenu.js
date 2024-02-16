90c64fdfde4d88c06b4cf71f9178060f58d8602e
Serum0017 <96929271+Serum0017@users.noreply.github.com>
2024-02-16 11:52:15 -0800

    removed back button, now click anywhere to go back

--- editor/spritemenu.js
+++ editor/spritemenu.js
@@ -1,5 +1,15 @@
 const newEntityContainer = document.getElementById('newentitybg');
 
+newEntityContainer.onclick = () => {
+    // for peace of mind
+    if(newEntityContainer.classList.contains("hidden")) return;
+
+    while(newEntityContainer.firstChild){
+        newEntityContainer.firstChild.remove();
+    }
+    newEntityContainer.classList.add('hidden');
+}
+
 const newEntityBtn = document.getElementById('newentity');
 newEntityBtn.onclick = () => {
   // const newName = prompt('enter workspace');
@@ -59,7 +69,7 @@ newEntityBtn.onclick = () => {
   }
 
   // new workspace btn
-  {
+//   {
     const newBtn = document.createElement('button');
     newBtn.type = 'button';
     newBtn.classList.add('spriteworkspacebutton');
@@ -94,41 +104,41 @@ newEntityBtn.onclick = () => {
     }
 
     newEntityContainer.appendChild(newBtn);
-  }
+//   }
 
   // back button
-  {
-    const newBtn = document.createElement('button');
-    newBtn.type = 'button';
-    newBtn.classList.add('spriteworkspacebutton');
+//   {
+//     const newBtn = document.createElement('button');
+//     newBtn.type = 'button';
+//     newBtn.classList.add('spriteworkspacebutton');
 
-    // new image
-    const img = document.createElement('img');
-    img.src = `data:image/svg+xml,<%3Fxml version="1.0" encoding="utf-8"%3F><svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M13.7071 1.29289C14.0976 1.68342 14.0976 2.31658 13.7071 2.70711L12.4053 4.00896C17.1877 4.22089 21 8.16524 21 13C21 17.9706 16.9706 22 12 22C7.02944 22 3 17.9706 3 13C3 12.4477 3.44772 12 4 12C4.55228 12 5 12.4477 5 13C5 16.866 8.13401 20 12 20C15.866 20 19 16.866 19 13C19 9.2774 16.0942 6.23349 12.427 6.01281L13.7071 7.29289C14.0976 7.68342 14.0976 8.31658 13.7071 8.70711C13.3166 9.09763 12.6834 9.09763 12.2929 8.70711L9.29289 5.70711C9.10536 5.51957 9 5.26522 9 5C9 4.73478 9.10536 4.48043 9.29289 4.29289L12.2929 1.29289C12.6834 0.902369 13.3166 0.902369 13.7071 1.29289Z" fill="%230F1729"/></svg>`;
-    img.classList.add('spriteworkspaceimg');
-    // black bg
-    // img.style.background = 'rgba(0,0,0,0.2)';
-    // img.style.borderRadius = '12px';
-    newBtn.appendChild(img);
+//     // new image
+//     const img = document.createElement('img');
+//     img.src = `data:image/svg+xml,<%3Fxml version="1.0" encoding="utf-8"%3F><svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M13.7071 1.29289C14.0976 1.68342 14.0976 2.31658 13.7071 2.70711L12.4053 4.00896C17.1877 4.22089 21 8.16524 21 13C21 17.9706 16.9706 22 12 22C7.02944 22 3 17.9706 3 13C3 12.4477 3.44772 12 4 12C4.55228 12 5 12.4477 5 13C5 16.866 8.13401 20 12 20C15.866 20 19 16.866 19 13C19 9.2774 16.0942 6.23349 12.427 6.01281L13.7071 7.29289C14.0976 7.68342 14.0976 8.31658 13.7071 8.70711C13.3166 9.09763 12.6834 9.09763 12.2929 8.70711L9.29289 5.70711C9.10536 5.51957 9 5.26522 9 5C9 4.73478 9.10536 4.48043 9.29289 4.29289L12.2929 1.29289C12.6834 0.902369 13.3166 0.902369 13.7071 1.29289Z" fill="%230F1729"/></svg>`;
+//     img.classList.add('spriteworkspaceimg');
+//     // black bg
+//     // img.style.background = 'rgba(0,0,0,0.2)';
+//     // img.style.borderRadius = '12px';
+//     newBtn.appendChild(img);
 
-    // label
-    const l = document.createElement('p');
-    l.innerText = 'Back';
-    l.classList.add('spriteworkspacelabel');
-    newBtn.appendChild(l);
+//     // label
+//     const l = document.createElement('p');
+//     l.innerText = 'Back';
+//     l.classList.add('spriteworkspacelabel');
+//     newBtn.appendChild(l);
 
-    newBtn.style.left = window.innerWidth / 2 + 'px';
-    newBtn.style.top = window.innerHeight / 2 + 'px';
+//     newBtn.style.left = window.innerWidth / 2 + 'px';
+//     newBtn.style.top = window.innerHeight / 2 + 'px';
 
-    newBtn.onclick = () => {
-      while(newEntityContainer.firstChild){
-        newEntityContainer.firstChild.remove();
-      }
-      newEntityContainer.classList.add('hidden');
-    }
+//     newBtn.onclick = () => {
+//       while(newEntityContainer.firstChild){
+//         newEntityContainer.firstChild.remove();
+//       }
+//       newEntityContainer.classList.add('hidden');
+//     }
 
-    newEntityContainer.appendChild(newBtn);
-  }
+//     newEntityContainer.appendChild(newBtn);
+//   }
 
   // Choose Sprite text
   const cs = document.createElement('p');
@@ -152,9 +162,9 @@ window.addEventListener("resize", () => {
         newBtn.style.top = window.innerHeight / 2 + Math.sin(angle) * minimumDimension * 0.3 + 'px';
     }
 
-    // back button
-    const newBtn = newEntityContainer.children[newEntityContainer.children.length-2];
+    // // back button
+    // const newBtn = newEntityContainer.children[newEntityContainer.children.length-2];
 
-    newBtn.style.left = window.innerWidth / 2 + 'px';
-    newBtn.style.top = window.innerHeight / 2 + 'px';
+    // newBtn.style.left = window.innerWidth / 2 + 'px';
+    // newBtn.style.top = window.innerHeight / 2 + 'px';
 })
